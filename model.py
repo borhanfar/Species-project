1b2d08e19141d68a76dc03a3198ae32842214c6c
borhanfar <mehdiborhanfar@gmail.com>
2021-08-01 11:39:38 +0100

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from sklearn import svm
 import pickle
 from pandas import read_csv
 df = read_csv('Fish.csv')
-df.rename(columns={'Length1':'Vertical_length(cm)', 'Length2':'Length(cm)', 'Length3':'Diagonal_length(cm)', 'Weight' : 'Weight(gr)', 'Height' : 'Height(cm)' , 'Width' : 'Width(cm)'}, inplace=True)
+df.rename(columns={'Length1':'Vertical_length(cm)', 'Length2':'Length(cm)', 'Length3':'Diagonal_length(cm)', 'Weight' : 'Weight(gr)', 'Height' : 'Height(Vertical_length without fins)(cm)' , 'Width' : 'Width(without gills)(cm)'}, inplace=True)
 X= df[['Weight(gr)','Vertical_length(cm)','Length(cm)','Diagonal_length(cm)','Height(cm)','Width(cm)']]
 y= df[['Species']]
 model = svm.SVC(kernel='linear',C=30,gamma= 'auto',degree=3)
